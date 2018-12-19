b9179cacf8a57539f849e6423ad722368e24ffd0
Edward Dupre <edwd42@gmail.com>
2018-12-19 15:19:42 -0500

    add package.json and edit truffle.js

--- tokenzender-contract/truffle.js
+++ tokenzender-contract/truffle.js
@@ -12,7 +12,19 @@
  *   },
  */
 
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+// module.exports = {
+//   // See <http://truffleframework.com/docs/advanced/configuration>
+//   // to customize your Truffle configuration!
+// };
+
+require('babel-register');  
+require('babel-polyfill');  
+  
+module.exports = {  
+  networks: {  
+  development: {  
+  host: "127.0.0.1",  
+  port: 7545,  
+  network_id: "*" // Match any network id  
+  }  
+ }};
